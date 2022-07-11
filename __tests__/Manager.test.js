abbd9f5882abe3c3affe9c2b5af56783c2065fb0
Mo <mohamed.abdullahi@yahoo.com>
2022-07-11 01:49:26 -0700

    create Managers tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,22 @@
 const Manager = require('../lib/Manager');
+
+//Create PhoneNumber for Manager
+test('Create phoneNumber', () => {
+    const phoneNumber = 999;
+    const newManager = new Manager('Mohamed', 1, "mohamed@gmail.com", phoneNumber);
+    expect(newManager.phoneNumber).toBe(phoneNumber);
+});
+
+//Checks if getRole() returns managers role
+test('Checks if getRole() returns managers role', () => {
+    const role = 'Manager';
+    const newManager = new Manager('Mohamed', 1, 'mohamed@gmail.com', 999);
+    expect(newManager.getRole()).toBe(role);
+});
+
+//Checks if getPhoneNumber() returns managers number
+test('Checks if getPhoneNumber() returns managers number', () => {
+    const phoneNumber = 999;
+    const newManager = new Manager('Mohamed', 1, 'mohamed@gmail.com', phoneNumber);
+    expect(newManager.getPhoneNumber()).toBe(phoneNumber);
+});
