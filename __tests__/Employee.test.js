f4e50634ef4a7ce309dd9e416665ef6dd7db1e2e
Mo <mohamed.abdullahi@yahoo.com>
2022-07-11 01:01:30 -0700

    creat Engineers tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,47 +1,55 @@
-const { default: test } = require('node:test');
 const Employee = require('../lib/Employee');
 
+
+//Makes sure employee is an object
 test('employee object', () => {
     const newEmployee = new Employee();
     expect(typeof newEmployee).toBe('object');
 });
 
+//Creates name for employee
 test('create name with constructor', () => {
     const name = 'Mohamed';
     const newEmployee = new Employee(name);
     expect(newEmployee.name).toBe(name);
 });
 
+//Creates id for employee
 test('create id with constructor', () => {
     const id = 1;
     const newEmployee = new Employee('Mohamed', id);
     expect(newEmployee.id).toBe(id);
 });
 
+//Creates email for employee
 test('create email with constructor', () => {
     const email = 'mohamed@gmail.com';
     const newEmployee = new Employee('Mohamed', 1, email);
     expect(newEmployee.email).toBe(email);
 });
 
+//Checks if getName() returns Employee name
 test('checks if getName() returns Employees name', () => {
     const name = 'Mohamed';
     const newEmployee = new Employee(name);
     expect(newEmployee.getName()).toBe(name);
 });
 
+//Checks if getId() returns Employee Id
 test('checks if getId() returns Employees Id', () => {
     const id = '1';
     const newEmployee = new Employee('Mohamed', id);
     expect(newEmployee.getId()).toBe(id);
 });
 
+//Checks if getEmail() returns Employees Email
 test('checks if getEmail() returns Employees Email', () => {
     const email = 'mohamed@gmail.com';
     const newEmployee = new Employee('Mohamed', 1, email);
     expect(newEmployee.getEmail()).toBe(email);
 });
 
+//Checks if getRole() returns Employees Role
 test('checks if getRole() returns Employees role', () => {
     const role = 'Employee';
     const newEmployee = new Employee('Mohamed', 1, 'mohamed@gmail.com');
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,25 @@
+const { exportAllDeclaration } = require('@babel/types');
+const { default: test } = require('node:test');
 const Engineer = require('../lib/Engineer');
+
+//Creats GitHub Username
+test('Creates github username', () => {
+    const github = 'mohamed12';
+    const newEngineer = new Engineer('Mohamed', 1, 'mohamed@gmail.com', github);
+    expect(newEngineer.github).tobe(github);
+});
+
+//Checks if newEngineer.getRole() returns Engineers role
+test('Checks if getRole() returns engineers role', () => {
+    const role = 'Engineer';
+    const newEngineer = new Engineer('Mohamed', 1, 'mohamed@gmail.com', 'mohamed12');
+    expect(newEngineer.getRole()).tobe(role);
+});
+
+//Checks if newEngineer.getGithub() returns Engineers github
+test('Checks if getGithub() returns github username', () => {
+    const github = 'mohamed12';
+    const newEngineer = new Engineer('Mohamed', 1, 'mohamed@gmail.com', github);
+    expect(newEngineer.getGithub()).tobe(github);
+});
+
