0fcd227992cb09a926fda30dc10b156790e340d4
Mo <mohamed.abdullahi@yahoo.com>
2022-07-11 01:32:57 -0700

    create Interns tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,25 +1,23 @@
-const { exportAllDeclaration } = require('@babel/types');
-const { default: test } = require('node:test');
 const Engineer = require('../lib/Engineer');
 
 //Creats GitHub Username
 test('Creates github username', () => {
     const github = 'mohamed12';
     const newEngineer = new Engineer('Mohamed', 1, 'mohamed@gmail.com', github);
-    expect(newEngineer.github).tobe(github);
+    expect(newEngineer.github).toBe(github);
 });
 
 //Checks if newEngineer.getRole() returns Engineers role
 test('Checks if getRole() returns engineers role', () => {
     const role = 'Engineer';
     const newEngineer = new Engineer('Mohamed', 1, 'mohamed@gmail.com', 'mohamed12');
-    expect(newEngineer.getRole()).tobe(role);
+    expect(newEngineer.getRole()).toBe(role);
 });
 
 //Checks if newEngineer.getGithub() returns Engineers github
 test('Checks if getGithub() returns github username', () => {
     const github = 'mohamed12';
     const newEngineer = new Engineer('Mohamed', 1, 'mohamed@gmail.com', github);
-    expect(newEngineer.getGithub()).tobe(github);
+    expect(newEngineer.getGithub()).toBe(github);
 });
 
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,22 @@
 const Intern = require('../lib/Intern');
+
+//Create school name
+test('creates school', () => {
+    const school = 'University of Oregon';
+    const newIntern = new Intern('Mohamed', 1, 'mohamed@gmail.com', school);
+    expect(newIntern.school).toBe(school);
+});
+
+//Checks if newIntern.getRole() returns interns role
+test('Checks if getRole() returns interns role', () => {
+    const role = 'Intern';
+    const newIntern = new Intern('Mohamed', 1, 'mohamed@gmail.com', 'University of Oregon');
+    expect(newIntern.getRole()).toBe(role);
+});
+
+//Checks if newIntern.getSchool() returns interns school
+test('Checks if getSchool() returns interns school', () => {
+    const school = 'University of Oregon';
+    const newIntern = new Intern('Mohamed', 1, 'mohamed@gmail.com', school);
+    expect(newIntern.getSchool()).toBe(school);
+});
